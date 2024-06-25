a99de56966ed63bdbebbe68c024d471f6fdf9303
bs1081 <82935893+bs1081@users.noreply.github.com>
2024-06-25 11:39:10 +0900

    Initial commit

---
+++ test0313/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+const myHeading2 = document.querySelector('h2');
+myHeading2.textContent = 'Hello World!';
+
+document.querySelector('h3').textContent = '안녕하세요!';
